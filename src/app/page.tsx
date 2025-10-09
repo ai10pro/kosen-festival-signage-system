78aebae1f9f33228e041d1d1dad40a3e3890a04c
ai10pro <106215301+ai10pro@users.noreply.github.com>
2025-10-09 23:44:34 +0900

    update README.md and page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex flex-col items-center text-center justify-center w-full min-w-0 min-h-[calc(100dvh-2.75rem)] mx-auto max-w-7xl">
-      <h1 className="text-4xl font-extrabold mb-2">♥️ Hii</h1>
+      <h1 className="text-4xl font-extrabold mb-2">This is home</h1>
     </main>
   );
 }
