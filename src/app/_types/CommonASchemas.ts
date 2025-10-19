115a8d9030ff451c24aedc2e73a1c359595b344e
ai10pro <106215301+ai10pro@users.noreply.github.com>
2025-10-20 01:29:56 +0900

    :sparkles: LoginRequestSchemaの定義

---
+++ src/app/_types/CommonASchemas.ts
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+export const userNameSchema = z
+  .string()
+  .min(5, { message: "ユーザー名を入力してください" });
+
+export const passwordSchema = z
+  .string()
+  .min(8, { message: "パスワードは8文字以上で入力してください" });
